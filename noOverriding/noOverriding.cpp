5e6796cafa179aa080d38ae340467ca74d09acc4
hikmatyar22 <abiq5161@gmail.com>
2024-05-28 10:37:51 +0700

    membuat class baseclass dan membernya

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// class perent 
+// tambahkan final sesudah nama class
+// untuk mencegah adanya overriding
+
+class baseClass {
+public:
+	virtual void perkenalan()
+	{
+		cout << "Hallo saya function dari base class";
+	}
+};
+
+
+int main() {
+
+}
