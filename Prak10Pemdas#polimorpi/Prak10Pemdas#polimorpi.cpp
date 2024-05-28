90b46a7d601c66fe73cf59440b61d0a073e9328c
hikmatyar22 <abiq5161@gmail.com>
2024-05-28 10:22:33 +0700

    membuat class seseorang dan membernya bersifat public

---
+++ Prak10Pemdas#polimorpi/Prak10Pemdas#polimorpi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+	// pure virtual function
+	virtual void pesan() = 0;
+	// virtual function biasa
+	// virtual void pesan() {
+	//	   cout << "Pesan dari seseorang" << endl;
+	// }
+};
+
+
+int main() {
+
+}
